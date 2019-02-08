946c69ff9b0b55f6e09b45cc90d4358d90781656
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2019-02-08 10:29:30 +0200

    add icon to listtile

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -7,20 +7,79 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
     return new MaterialApp(
-        home: new Scaffold(appBar: AppBar(), body: Center()));
+        home: new Scaffold(
+            appBar: AppBar(
+              title: Text('Random Generator'),
+
+            ),
+            body: Center(
+              child: new Random(),
+            )
+        )
+    );
   }
 }
-
 class Random extends StatefulWidget {
   @override
   _RandomState createState() => _RandomState();
 }
 
 class _RandomState extends State<Random> {
+  final List<WordPair> _suggestions = <WordPair>[];
+
+  final Set<WordPair> _loved = Set<WordPair>();
+
+  final TextStyle customStyle = TextStyle(
+      fontSize: 30.0,
+      color: Colors.blueGrey
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return _generateRandom();
   }
+
+  Widget _generateRandom() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          final int index = i ~/ 2;
+          if (i.isOdd) {
+            return Divider();
+          }
+          if (index >= _suggestions.length) {
+            // generateWordPairs() is a method inside WordPair generate list of wordpairs
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final isLoved = _loved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+      ),
+      trailing: new Icon(
+        isLoved ? Icons.favorite : Icons.favorite_border,
+        color: isLoved ? Colors.red : null,
+      )
+      ,
+      onTap: () {
+        // used to update UI by calling build method
+        setState(() {
+          // check if it loved then remove it otherwise add it to loved Set
+          if (isLoved) {
+            _loved.remove(pair);
+          } else {
+            _loved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
 }
